bc0de5991cf2b7a58c8763e76d7f8f6d265a9237
suzuiiyo <she7ter@gmail.com>
2019-12-20 11:53:12 +0800

    静态方法

---
+++ StaticTry.java
@@ -0,0 +1,39 @@
+class StaticSuper{
+    static{
+        System.out.println("super static block");
+    }
+
+    StaticSuper(){
+        System.out.println("super constructor");
+    }
+}
+class StaticTests extends StaticSuper{
+    static int rand;
+
+    static{
+        rand = (int) (Math.random() * 6);
+        System.out.println("static block " + rand);
+    }
+
+    StaticTests(){
+        System.out.println("constructor");
+    }
+
+    StaticSuper(){
+        System.out.println("XXXXXXXXXXXX");
+        System.out.println("picapicachoo!");
+    }
+/*    public static void main(String[] args){
+        System.out.println("in main");
+        StaticTests st = new StaticTests();
+        st.StaticSuper();
+    }*/
+}
+
+public class StaticTry{
+    public static void main(String[] args){
+        System.out.println("in main");
+        StaticTests st = new StaticTests();
+        st.StaticSuper();
+    }
+}
