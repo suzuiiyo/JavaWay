496f30191ebec1b1614a5afc5be9d53d12400793
suzuiiyo <she7ter@gmail.com>
2020-04-19 23:59:42 +0800

    object类

--- how2j/charactor/Hero9.java
+++ how2j/charactor/Hero9.java
@@ -4,7 +4,7 @@ public class Hero9 {
     public String name;
     protected float hp;
 
-    public boolean equals(Object 0){
+    public boolean equals(Object o){
         if(o instanceof Hero){
             Hero h = (Hero) o;
             return this.hp == h.hp;
@@ -12,19 +12,19 @@ public class Hero9 {
         return false;
     }
 
-    public String toString(){
+    /*public String toString(){
         return name;
     }
 
     public void finalize(){
         System.out.println("这个英雄正在被回收");
-    }
+    }*/
 
     public static void main(String[] args){
-        Hero h;
+        /*Hero h;
         for (int i =0; i <100000; i++){
             h = new Hero();
-        }
+        }*/
 
         Hero h1 = new Hero();
         h1.hp = 300;
@@ -35,5 +35,7 @@ public class Hero9 {
 
         System.out.println(h1.equals(h2));
         System.out.println(h1.equals(h3));
+        System.out.println(h1==h2);
+        System.out.println(h1==h3);
     }
 }
--- how2j/property/Item.java
+++ how2j/property/Item.java
@@ -8,18 +8,38 @@ public class Item{
         System.out.println("使用物品");
     }
 
+    public String toString(){
+        return (name + price);
+    }
+
+    public void finalize(){
+        System.out.println("当前对象正在被回收");
+    }
+
+    public boolean equals(Object o){
+        if(o instanceof Item){
+            Item h = (Item) o;
+            return this.price == h.price;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args){
         Item one = new Item();
-        one.name = "????";
+        one.name = "红瓶";
         one.price = 50;
 
         Item two = new Item();
-        two.name = "????";
-        two.price = 300;
+        two.name = "蓝瓶";
+        two.price = 50;
 
         Item three = new Item();
-        three.name = "?¤??";
+        three.name = "卷轴";
         three.price = 350;
         System.out.println("what's up");
+
+        System.out.println(one.equals(two));
+        System.out.println(one.equals(three));
     }
 }
