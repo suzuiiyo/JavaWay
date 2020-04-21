285c480bae1c912e40991db3e5939f0a26083029
suzuiiyo <she7ter@gmail.com>
2020-04-21 17:18:00 +0800

    质数判断

---
+++ how2j/digit/JudgePrime.java
@@ -0,0 +1,28 @@
+package digit;
+import java.util.Scanner;
+
+public class JudgePrime {
+    public void judgePrime(int m){
+        boolean flag = true;
+        for(int h=2; h<=Math.sqrt(m); h++){
+            if(m%h==0&&h>=2){
+                System.out.println(m + "不是质数");
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            System.out.println(m + "是质数");
+        }
+    }
+
+    public static void main(String[] args) {
+        int m;
+        Scanner s = new Scanner(System.in);
+        System.out.println("请输入一个大于1的正整数: ");
+        m = s.nextInt();
+        s.close();
+        JudgePrime num = new JudgePrime();
+        num.judgePrime(m);
+    }
+}
