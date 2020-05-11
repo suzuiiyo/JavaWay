db64f535006af5904219600b29cd8be6be57b0dc
suzuiiyo <she7ter@gmail.com>
2020-05-11 23:46:13 +0800

    文件分割与合并

--- how2j/file/TestSplitFile.java
+++ how2j/file/TestSplitFile.java
@@ -1,9 +1,11 @@
 package file;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
+//import java.io.PrintStream;
 
 public class TestSplitFile {
     public static void main(String[] args){
@@ -12,28 +14,37 @@ public class TestSplitFile {
         String end = ".gif";
         String path2;
 
-        double l = Math.pow(2, 10)*100;   //文件长度
+        double size = Math.pow(2, 10)*100;   //文件长度
         //long n=10;   //设置分割总数
         File f = new File(path1 + name + end);    //转换成文件对象
         File f2;
-        InputStream in = null;              //申明字节流
-        PrintStream out = null;             //申明打印流
+        byte[] byteArr = new byte[(int)f.length()];
+        InputStream in = null;              //申明输入流
+        FileOutputStream out = null;             //申明输出流
         try{
             in = new FileInputStream(f);    //将文件f转换成字节输入流
             int tempbyte;                   //字节流返回值,为-1时字节流结束
             int i = 0;
-            for(i=0; i<=(int)(f.length()/l); i++){
+            int count =0;
+            for(i=0; i<=(int)(f.length()/size); i++){
                 path2 = path1 + name + i + end;
                 f2 = new File(path2);
                 if(!f2.exists()){
                     f2.createNewFile();
                 }
-                out = new PrintStream(f2);
+                out = new FileOutputStream(f2);
+                count = i*(int)size;
                 
-                while((tempbyte = in.read()) != -1){   //数据的下一个字节，如果达到流的末尾， 则返回-1 
-                    out.println((char)tempbyte);   //打印的什么鬼
-                    System.out.write(tempbyte);
-                    if(f2.length()>l)
+                while((tempbyte = in.read()) != -1){
+                    if(i<18) {  //数据的下一个字节，如果达到流的末尾， 则返回-1 
+                        out.write(byteArr, count, (int)size-1);   //从byte数组写入bye到输出流
+                        System.out.write(tempbyte);
+                    }
+                    else if(i==18){
+                        out.write(byteArr, count, byteArr.length-count);   //从byte数组写入bye到输出流
+                        System.out.write(tempbyte);
+                    }
+                    if(f2.length()>size)
                         break;                  //子文件到达指定大小就创建下一个文件;
                 }
             }
--- how2j/file/mergeFiles.java
+++ how2j/file/mergeFiles.java
@@ -12,29 +12,26 @@ public class mergeFiles {
         String dirHead = "d:/Studio/JavaWay/AC/DC/";
         String name = "scared";
         String dirTail = ".gif";
-        File f = new File(dirHead);
+        //File f = new File(dirHead);
         File f1 = new File(dirHead + name + dirTail);
-        File[] fs = f.listFiles();
+        //File[] fs = f.listFiles();
         InputStream in = null;
         PrintStream fout = null;
         try{
             int tempbyte;
-            //if(!f2.exists()){
-            //    f2.createNewFile();
-           // }
             File f2 = new File(dirHead + "scareME" + dirTail);
             if(!f2.exists()){
                 f2.createNewFile();
             }
+            fout = new PrintStream(f2);
             for(int i=0; i<(int)f1.length()/size; i++){
-                if(fs[i].length() == size){
-                    in = new FileInputStream(fs[i]);
+                //if(fs[i].length() == size){
+                File f3 = new File(dirHead + name + i + dirTail);
+                in = new FileInputStream(f3);
                 
-                    fout = new PrintStream(f2);
-                    while((tempbyte = in.read()) != -1){
-                        fout.println((char)tempbyte);   //打印的什么鬼
+                while((tempbyte = in.read()) != -1){
+                    fout.println((char)tempbyte);   //读取in里的byte到输出流
                     System.out.write(tempbyte);
-                    }
                 }
             }
             fout.flush();
