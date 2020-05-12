093cc439ac84269378a18e3f0fcc03aae647a4eb
suzuiiyo <she7ter@gmail.com>
2020-05-12 11:29:05 +0800

    f分割文件并还原

--- how2j/file/TestSplitFile.java
+++ how2j/file/TestSplitFile.java
@@ -14,7 +14,7 @@ public class TestSplitFile {
         String end = ".gif";
         String path2;
 
-        double size = Math.pow(2, 10)*100;   //文件长度
+        double size = Math.pow(2, 10)*100;   //文件长度100K
         //long n=10;   //设置分割总数
         File f = new File(path1 + name + end);    //转换成文件对象
         File f2;
@@ -22,10 +22,10 @@ public class TestSplitFile {
         InputStream in = null;              //申明输入流
         FileOutputStream out = null;             //申明输出流
         try{
-            in = new FileInputStream(f);    //将文件f转换成字节输入流
-            int tempbyte;                   //字节流返回值,为-1时字节流结束
+            in = new FileInputStream(f);    //将文件f转换成字节流
+            byteArr = InputStreamtoByteArray.read(in);
             int i = 0;
-            int count =0;
+            int len =0;
             for(i=0; i<=(int)(f.length()/size); i++){
                 path2 = path1 + name + i + end;
                 f2 = new File(path2);
@@ -33,20 +33,18 @@ public class TestSplitFile {
                     f2.createNewFile();
                 }
                 out = new FileOutputStream(f2);
-                count = i*(int)size;
+                len = i*(int)size;              //记录每byteArr写入f2后的byte的位置
                 
-                while((tempbyte = in.read()) != -1){
-                    if(i<18) {  //数据的下一个字节，如果达到流的末尾， 则返回-1 
-                        out.write(byteArr, count, (int)size-1);   //从byte数组写入bye到输出流
-                        System.out.write(tempbyte);
-                    }
-                    else if(i==18){
-                        out.write(byteArr, count, byteArr.length-count);   //从byte数组写入bye到输出流
-                        System.out.write(tempbyte);
-                    }
-                    if(f2.length()>size)
-                        break;                  //子文件到达指定大小就创建下一个文件;
+                if(i<(int)(f.length()/size)) {  //数据的下一个字节，如果达到流的末尾， 则返回-1 
+                    out.write(byteArr, len, (int)size);   //从byte数组写入bye到输出流
+                    //System.out.println("非尾巴：" + len);
                 }
+                else if(i==(int)(f.length()/size)){
+                    out.write(byteArr, len, byteArr.length-len);   //从byte数组写入bye到输出流
+                    //System.out.println("尾巴：" + (byteArr.length-len));
+                }
+                if(f2.length()>size)
+                    break;                  //子文件到达指定大小就创建下一个文件;
             }
             out.flush();
             out.close();
--- how2j/file/mergeFiles.java
+++ how2j/file/mergeFiles.java
@@ -2,9 +2,9 @@ package file;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 
 public class mergeFiles {
     public static void main(String[] arg) {
@@ -14,25 +14,23 @@ public class mergeFiles {
         String dirTail = ".gif";
         //File f = new File(dirHead);
         File f1 = new File(dirHead + name + dirTail);
+        byte[] byteArr = new byte[(int)f1.length()];
         //File[] fs = f.listFiles();
         InputStream in = null;
-        PrintStream fout = null;
+        FileOutputStream fout = null;
         try{
-            int tempbyte;
             File f2 = new File(dirHead + "scareME" + dirTail);
             if(!f2.exists()){
                 f2.createNewFile();
             }
-            fout = new PrintStream(f2);
-            for(int i=0; i<(int)f1.length()/size; i++){
+            fout = new FileOutputStream(f2);
+
+            for(int i=0; i<=(int)(f1.length()/size); i++){
                 //if(fs[i].length() == size){
                 File f3 = new File(dirHead + name + i + dirTail);
                 in = new FileInputStream(f3);
-                
-                while((tempbyte = in.read()) != -1){
-                    fout.println((char)tempbyte);   //读取in里的byte到输出流
-                    System.out.write(tempbyte);
-                }
+                byteArr = InputStreamtoByteArray.read(in);
+                fout.write(byteArr);
             }
             fout.flush();
             fout.close();
