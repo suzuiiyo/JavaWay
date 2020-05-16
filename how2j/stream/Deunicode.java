68686583e71c4436c8e18b9f6d0f979be7425a76
suzuiiyo <she7ter@gmail.com>
2020-05-16 23:14:54 +0800

    编码解码

---
+++ how2j/stream/Deunicode.java
@@ -0,0 +1,45 @@
+package stream;
+
+import java.io.UnsupportedEncodingException;
+
+public class Deunicode {
+
+    /**
+*?将?16?进制字符串转换成汉字
+*/
+    public static void main(String[] args){
+        String str = "6C49";
+        String hanzi = deUnicode(str);
+        System.out.printf("对应的汉字是%s:", hanzi);
+    }
+
+    public static String deUnicode(String str) {
+        byte[] bytes = new byte[str.length() / 2];
+        byte tempByte = 0;
+        byte tempHigh = 0;
+        byte tempLow = 0;
+        for (int i = 0, j = 0; i < str.length(); i += 2, j++) {
+            //System.out.println((int)str.charAt(i));    //返回的是索引为i的元素的(ASCII码)
+            tempByte = (byte) (((int) str.charAt(i)) & 0xff);
+            if (tempByte >= 48 && tempByte <= 57) {
+                tempHigh = (byte) ((tempByte - 48) << 4);
+            } else if (tempByte >= 97 && tempByte <= 101) {
+                tempHigh = (byte) ((tempByte - 97 + 10) << 4);
+            }
+            tempByte = (byte) (((int) str.charAt(i + 1)) & 0xff);
+            if (tempByte >= 48 && tempByte <= 57) {
+                tempLow = (byte) (tempByte - 48);
+            } else if (tempByte >= 97 && tempByte <= 101) {
+                tempLow = (byte) (tempByte - 97 + 10);
+            }
+            bytes[j] = (byte) (tempHigh | tempLow);
+        }
+        String result = null;
+        try {
+            result = new String(bytes, "GBK");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
--- how2j/stream/EncodeOfChinese.java
+++ how2j/stream/EncodeOfChinese.java
@@ -1,18 +1,35 @@
 package stream;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
+
 public class EncodeOfChinese {
     public static void main(String[] args){
-        byte[] ben = {'E5', 'B1', '8C'};
-        showChinese();
-    }
-    private static void showChinese(String str){
-
+        byte[] bb = {(byte)0xA2, (byte)0xB9, (byte)0x9C}; 
+        showChinese(bb);
     }
 
-    private static void showChinese(byte[] byteArr,  String encode){
-        try{
-            System.out.println("数字：\");
+    private static void showChinese(byte[] bb){
+        String[] encodes = {"BIG5", "GBK", "GB2312", "UTF-8", "UTF-16", "UTF-32"};
+        for(String encode : encodes){
+            showChinese(bb, encode);
         }
     }
-    
+
+    private static void showChinese(byte[] bb,  String encode){
+        try{
+            System.out.printf("编码: \"%s在编码方式%s下的汉字是%n", bb, encode);
+            //String[] encodes = {"BIG5", "GBK", "GB2312", "UTF-8", "UTF-16", "UTF-32"};
+            Charset charset = Charset.forName(encode);
+            ByteBuffer buf = ByteBuffer.wrap(bb);
+            CharBuffer charBuffer = charset.decode(buf);
+            String chi = charBuffer.toString();
+            System.out.println();
+            System.out.println(chi);
+        }catch(UnsupportedCharsetException e) {
+            System.out.printf("UnsupportedCharsetException: %s编码方式无法解析编码%s\n", encode, bb);
+        }
+    }
 }
