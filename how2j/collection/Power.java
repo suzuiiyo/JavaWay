6685b3947fd299d797b38c4709e3313097dc9a74
suzuiiyo <she7ter@gmail.com>
2020-05-23 22:51:22 +0800

    泛型Generic

---
+++ how2j/collection/Power.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class Power {
+    public static void main(String[] args){
+        ArrayList que = new ArrayList();
+        int sum  = 0;
+        for(int i=0; i<100; i++){
+            while(sum < 100){
+                sum += i;
+                que.add(i);
+                break;
+            }
+        }
+        System.out.println(que);
+        System.out.println(sum);
+    }
+}
