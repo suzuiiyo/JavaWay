6b0be9b175cca62912a90418ad4db02f46896bf1
suzuiiyo <she7ter@gmail.com>
2020-05-23 22:49:48 +0800

    矩阵幂

---
+++ Matrix/testMatrix.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+{
+    main() {
+        const in N=100;
+        int c[N][N];
+
+        void multi(int a [][N], int b[][N], int n) //n «æÿ’Û¥Û–°, n<N
+        {
+            meset(c, 0, sizeof c);
+            for(int i=1; i<=n; i++){
+                for(int i=1; i<=n; i++){
+                    for(int k=1; k<=n; k++)
+                        c[i][j] += a[i][k]*b[k][j];
+            }
+        }
+    }
+}
