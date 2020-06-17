b68da8ffbbf6792bc0387a1343385b251e7c55ac
suzuiiyo <she7ter@gmail.com>
2020-06-17 23:42:17 +0800

    线程安全

--- how2j/collection/MyStack.java
+++ how2j/collection/MyStack.java
@@ -1,22 +1,36 @@
 package collection;
 
 import java.util.LinkedList;
-
 import charactor.Hero;
 
-public class MyStack implements Stack {
+public class MyStack<T> implements Stack<T> {
     LinkedList<Hero> heros = new LinkedList<Hero>();
+    //Stack<Hero> heros = new Stack<Hero>();
     //插入的时候,加上synchronized，同步对象是当前实例
     public synchronized void push(Hero h){
+        try{
+            this.wait();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
         heros.addLast(h);
     }
 
-    public synchronized Hero pull(){
+    public synchronized Hero pull() {
+        try{
+            this.wait();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
         return heros.removeLast();
     }
 
     //查看没必要加上synchronized，因为不修改数据
     public Hero peek(){
         return heros.getLast();
+    }
+
+    public int size(){
+        return heros.size();
     }
 }
