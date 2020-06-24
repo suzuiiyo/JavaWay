e2afcfb752982f004b0433e1a2cb0efff0f5c641
suzuiiyo <she7ter@gmail.com>
2020-06-25 02:08:42 +0800

    导入mysql驱动包

--- how2j/JDBC/TestJDBC.java
+++ how2j/JDBC/TestJDBC.java
@@ -1,4 +1,4 @@
-package JDBC;
+package jdbc;
 
 public class TestJDBC{
     public static void main(String[] args) {
@@ -6,7 +6,7 @@ public class TestJDBC{
             //驱动类com.mysql。jdbc.Driver
             //就在mysql-connector-java-***.jar
             //如果忘记第一个步骤的导包,就会抛出ClassNotFoundException
-            Class.forName("come.mysql-jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");    //老驱动是com.mysql.jdbc.Driver(废弃),新驱动是com.mysql.cj.jdbc.Driver
             System.out.println("数据库驱动加载成功!");
         }catch(ClassNotFoundException e){
             e.printStackTrace();
