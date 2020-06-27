077a6de4c885ea184561164514915df3547c04bd
suzuiiyo <she7ter@gmail.com>
2020-06-27 19:13:22 +0800

    autoClose

--- how2j/JDBC/TestTryWithResource.java
+++ how2j/JDBC/TestTryWithResource.java
@@ -17,12 +17,12 @@ public class TestTryWithResource {
         
         try (Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?chacracterEncoding=UTF-8&serverTimezone=UTC", "root", "admin"); Statement s = c.createStatement();)
         {
-            for(int i=0; i<100; i++){
+            for(int i=7; i<30; i++){
                 float hp = (float)Math.random()*1000;
                 float damage = (float)Math.random()*1000;
-                String sql = "insert into hero values(null," + "'巫医'" + "," + hp + "," + damage + ")";
+                String sql = "insert into hero values(" + i +", " + "'巫医'" + "," + hp + "," + damage + ")";
                 s.execute(sql);
-                System.out.println("第" + i +"次插入成功！");
+                System.out.println("第" + (i-5) +"次插入成功！");
             }
         }
         catch(SQLException e){
