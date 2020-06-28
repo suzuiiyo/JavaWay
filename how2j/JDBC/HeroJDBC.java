836c81ae43e8fcd52f83f2632008b0f02a74ecb4
suzuiiyo <she7ter@gmail.com>
2020-06-28 21:00:26 +0800

    封装JDBCHero类

--- how2j/JDBC/HeroJDBC.java
+++ how2j/JDBC/HeroJDBC.java
@@ -1,8 +1,44 @@
 package jdbc;
 
 public class HeroJDBC {
-    public int id;
-    public String name;
-    public float hp;
-    public int damage;
+    private int id;
+    private String name;
+    private float hp;
+    private int damage;
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setHp(Float hp){
+        this.hp = hp;
+    }
+
+    public Float getHp(){
+        return this.hp;
+    }
+
+    public void setDamage(int damage){
+        this.damage = damage;
+    }
+
+    public int getDamage(){
+        return this.damage;
+    }
+    public String toString() {
+        String toStr = "id:" + id + ", name:" + name + ", hp:" + hp + ", damage:" + damage;
+        return toStr;
+    }
 }
