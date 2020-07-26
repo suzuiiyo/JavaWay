edf3f54669936e746241a1ddb3cc653d36fe07f3
suzuiiyo <she7ter@gmail.com>
2020-07-26 21:17:33 +0800

    动作事件

--- how2j/gui/TestActionListener.java
+++ how2j/gui/TestActionListener.java
@@ -7,7 +7,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import javafx.event.ActionEvent;
 
 public class TestActionListener {
     public static void main(String[] args) {
