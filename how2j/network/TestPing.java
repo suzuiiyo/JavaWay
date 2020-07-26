8f99d17957871a8dd90bca1856315c458151fea3
suzuiiyo <she7ter@gmail.com>
2020-07-26 21:18:08 +0800

    执行ping命令

---
+++ how2j/network/TestPing.java
@@ -0,0 +1,33 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestPing {
+    /*public static void main(String[] args) throws IOException {
+        Process p = Runtime.getRuntime().exec("ping " + "192.168.2.106");
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while((line = br.readLine()) != null){
+            if(line.length() != 0)
+                sb.append(line + "\r\n");
+        }
+        System.out.println("本次指令返回的消息是: ");
+        System.out.println(sb.toString());
+    }*/
+
+    public static void main(String[] args) throws IOException {
+        Process p = Runtime.getRuntime().exec("ping " + "192.168.2.106");
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while((line = br.readLine()) != null){
+            if(line.length() != 0)
+                sb.append(line + "\r\n");
+        }
+        System.out.println("本次指令返回的消息是: ");
+        System.out.println(sb.toString());
+    }
+}
