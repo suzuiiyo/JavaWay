4571b732221bc1f6f2079f9c51fcd3f957e8286f
suzuiiyo <she7ter@gmail.com>
2020-07-30 08:18:14 +0800

    UpdateAndriod数据库

--- how2j/JDBC/UpdateAndriodDatabase.java
+++ how2j/JDBC/UpdateAndriodDatabase.java
@@ -9,13 +9,13 @@ public class UpdateAndriodDatabase {
         DbUtil dul = new DbUtil();
         Connection conn = dul.getCon();
         String sqlStr = "insert into dictionary values(null, ?, ?)";
-        String[] receive = {"你好", "你叫什么", "打你哦", "同志", "你做乜啊"};
-        String[] response = {"好你妹", "你想泡我啊", "来啊, 来打我啊，不打是小狗", "关你p事额"};
-        for(int i=0; i<4; i++){
+        String[] receive = {"你好", "你叫什么", "打你哦", "同志", "你做乜啊", "123"};
+        String[] response = {"好你妹", "你想泡我啊", "来啊, 来打我啊，不打是小狗", "达瓦里希", "关你p事额", "机器人"};
+        for(int i=0; i<6; i++){
             try(PreparedStatement ps = conn.prepareStatement(sqlStr)){
                 ps.setString(1, receive[i]);
                 ps.setString(2, response[i]);
-                ps.execute();
+                ps.executeUpdate();
             }catch(SQLException e){
                 e.printStackTrace();
             }
