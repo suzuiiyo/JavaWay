6edac81eba20b151e6136ffbe3492c26c78ba1a3
suzuiiyo <she7ter@gmail.com>
2020-08-04 09:58:15 +0800

    反射切换业务

---
+++ how2j/reflection/TestService.java
@@ -0,0 +1,8 @@
+package reflection;
+
+public class TestService {
+    public static void main(String[] args) {
+        //new Service1().doService1();
+        new Service2().doService2();
+    }
+}
