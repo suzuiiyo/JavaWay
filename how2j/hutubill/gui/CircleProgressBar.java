f44d000e5771e09426f23166cab30a10919dd62d
suzuiiyo <she7ter@gmail.com>
2020-08-14 09:50:04 +0800

    环形进度条

---
+++ how2j/hutubill/gui/CircleProgressBar.java
@@ -0,0 +1,97 @@
+package hutubill.gui;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JPanel;
+
+public class CircleProgressBar extends JPanel {
+    private int miniumProgress;
+    private int maxiumProgress;
+
+    private int progress;
+    private String progressText;
+
+    private Color backgroundColor;
+    private Color foregroundColor;
+
+    public CircleProgressBar(){
+        miniumProgress = 0;
+        maxiumProgress = 100;
+        progressText   = "0%";
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D graphics2d = (Graphics2D) g;
+        //¿ªÆô¿¹¾â³Ý
+        graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int x        = 0;
+        int y        = 0;
+        int width    = 0;
+        int height   = 0;
+        int fontSize = 0;
+        if(getWidth() >= getHeight()) {
+            x        = (getWidth() - getHeight()) / 2 + 25;
+            y        = 25;
+            width    = getHeight() - 50;
+            height   = getHeight() - 50;
+            fontSize = getWidth() / 8;
+        }else{
+            x        = 25;
+            y        = (getHeight() - getWidth()) / 2 + 25;
+            width    = getWidth() - 50;
+            height   = getWidth() - 50;
+            fontSize = getHeight() / 8;
+        }
+        graphics2d.setStroke(new BasicStroke(20.0f));
+        graphics2d.setColor(backgroundColor);
+        graphics2d.drawArc(x, y, width, height, 0, 360);
+        graphics2d.setColor(foregroundColor);
+        graphics2d.drawArc(x, y, width, height, 90, -(int)(360 * ((progress * 1.0) / (maxiumProgress
+                - miniumProgress))));
+        graphics2d.setFont(new Font("ºÚÌå", Font.BOLD, fontSize));
+        FontMetrics fontMetrics   = graphics2d.getFontMetrics();
+        int         digitalWidth  = fontMetrics.stringWidth(progressText);
+        int         digitalAscent = fontMetrics.getAscent();
+        graphics2d.setColor(foregroundColor);
+        graphics2d.drawString(progressText, getWidth() / 2 - digitalWidth / 2, getHeight() / 2 + digitalAscent / 2);
+    }
+
+    public int getProgress(){
+        return progress;
+    }
+
+    public void setProgress(int progress){
+        if(progress >= miniumProgress && progress <= maxiumProgress)
+            this.progress = progress;
+        if(progress > maxiumProgress)
+            this.progress = maxiumProgress;
+        
+        this.progressText = String.valueOf(progress + "%");
+        this.repaint();
+    }
+
+    public Color getBackgrColor(){
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor){
+        this.backgroundColor = backgroundColor;
+        this.repaint();
+    }
+
+    public Color getForegroundColor(){
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(Color foregroundColor){
+        this.foregroundColor = foregroundColor;
+        this.repaint();
+    }
+}
