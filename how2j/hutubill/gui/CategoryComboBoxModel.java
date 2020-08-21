75f70bc9feed73db3eb22485d2bcf4bd494fb601
suzuiiyo <she7ter@gmail.com>
2020-08-21 23:35:03 +0800

    分类下拉框模型

---
+++ how2j/hutubill/gui/CategoryComboBoxModel.java
@@ -0,0 +1,59 @@
+package hutubill.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.event.ListDataListener;
+
+public class CategoryComboBoxModel implements ComboBoxModel<String> {
+    public List<String> cs = new ArrayList<>();
+
+    String c;
+
+    //用于原型数据显示
+    public CategoryComboBoxModel() {
+        cs.add("餐饮");
+        cs.add("交通");
+        cs.add("住宿");
+        cs.add("话费");
+        c = cs.get(0);
+    }
+
+    @Override
+    public int getSize() {
+        //TODO 获取下拉框的大小,就是前面定义的字符串集合的大小
+        return cs.size();
+    }
+
+    @Override
+    public String getElementAt(int index) {
+        // TODO 获取指定位置的数据
+        return cs.get(index);
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setSelectedItem(Object anItem) {
+        // TODO 当界面上选中了某一个下拉框项，就会调用这个方法
+        c = (String)anItem;
+
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        // TODO Auto-generated method stub
+        return c;
+    }
+}
