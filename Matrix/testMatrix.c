1aeda89ed4aa025fcb84d3932124f3049e5b4632
suzuiiyo <she7ter@gmail.com>
2020-08-27 16:50:14 +0800

    矩阵幂

--- Matrix/testMatrix.c
+++ Matrix/testMatrix.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-{
-    main() {
-        const in N=100;
+    int main() {
+        const int N=100;
         int c[N][N];
 
-        void multi(int a [][N], int b[][N], int n) //n是矩阵大小, n<N
+        void multi(int a[][N], int b[][N], int n) //n是矩阵大小, n<N
         {
             meset(c, 0, sizeof c);
             for(int i=1; i<=n; i++){
-                for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; i++){
                     for(int k=1; k<=n; k++)
                         c[i][j] += a[i][k]*b[k][j];
+                }
             }
         }
     }
-}
