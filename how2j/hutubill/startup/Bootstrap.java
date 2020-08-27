d6f9e002709f0838ac0eda4b207b5786b9504a3b
suzuiiyo <she7ter@gmail.com>
2020-08-27 16:49:03 +0800

    启动类

---
+++ how2j/hutubill/startup/Bootstrap.java
@@ -0,0 +1,28 @@
+package hutubill.startup;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import hutubill.gui.panel.GUIUtil;
+import hutubill.gui.panel.MainFrame;
+import hutubill.gui.panel.MainPanel;
+import hutubill.gui.panel.SpendPanel;
+
+public class Bootstrap {
+    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+        GUIUtil.useLNF();
+        //invokeAndWait  和 invokeAndLater
+        SwingUtilities.invokeAndWait(new Runnable(){
+			@Override
+			public void run() {
+				// TODO 启动！
+                MainFrame.instance.setVisible(true);
+                //MainPanel的实例上的woringkPanel上显示SpendPanel
+                //同理也可以显示其他的Panel,比如RecordPanel
+                //MainPanel.instance.workingPanel.show(CategoryPanel.instance);
+                MainPanel.instance.workingPanel.show(SpendPanel.instance);
+			}
+        });
+    }
+}
