e65ffb42bade20950469a83418c5612703d7d342
suzuiiyo <she7ter@gmail.com>
2020-09-09 19:56:39 +0800

    IntegerCache

---
+++ how2j/TestInteger.java
@@ -0,0 +1,10 @@
+public class TestInteger {
+    public static void main(String[] args) {
+        Integer a = 100;
+        Integer b = 100;
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
